2737bfbf0aa1ce9f25758785aeefbacbe93d8918
Roman Lenjo <roman25lenjo@gmail.com>
2025-06-07 01:37:53 +0300

    Integrate HubSpot forms

--- components/MetadataFilterLink.tsx
+++ components/MetadataFilterLink.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 type MetadataFilterLinkProps = {
@@ -15,7 +15,6 @@ const MetadataFilterLink = ({
   value,
   displayText,
 }: MetadataFilterLinkProps) => {
-  const pathname = usePathname(); // Should ideally be '/' for the gallery page
   const searchParams = useSearchParams();
 
   // Construct the new query string for filtering the main gallery page.
